cae79d2b215a446146188e8501198d36314fcce0
dipen <parihar.dipen@gmail.com>
2020-04-19 23:13:29 +0530

    database is added

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ const development = {
 };
 
 const testing = {
-  database: 'ddi3ntoi419oje',
-  username: 'cnchtehkgwzlpa',
-  password: 'd56bdde55bb74b3875bc849f638fd6d95654d406cf874629c24622a0cc6c53fd',
-  host: 'ec2-50-17-178-87.compute-1.amazonaws.com',
+  database: 'd88dkq9091mcrf',
+  username: 'qdfwokmotxmjbd',
+  password: '905b742eb55c5f7658ff6d39a277e27919f78e4c12e6ab3a2176416b9e404a1d',
+  host: 'ec2-23-20-129-146.compute-1.amazonaws.com',
   dialect: 'postgres',
 };
 
