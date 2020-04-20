1ab6b313b7e6310bae2a5de999bc48f58518c04a
Dipen Parihar <parihar.dipen@gmail.com>
2020-04-20 21:25:02 +0530

    migratation database false

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const privateRoutes = require('./routes/privateRoutes');
 const publicRoutes = require('./routes/publicRoutes');
 
 const config = {
-  migrate: true,
+  migrate: false,
   privateRoutes,
   publicRoutes,
   port: process.env.PORT || '2019',
