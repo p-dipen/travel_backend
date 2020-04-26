4265c276de9391c4f523b64f220fb336108f116f
dipen <parihar.dipen@gmail.com>
2020-04-26 18:10:20 +0530

    changes in db

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ const development = {
 };
 
 const testing = {
-  database: 'd88dkq9091mcrf',
-  username: 'qdfwokmotxmjbd',
-  password: '905b742eb55c5f7658ff6d39a277e27919f78e4c12e6ab3a2176416b9e404a1d',
-  host: 'ec2-23-20-129-146.compute-1.amazonaws.com',
+  database: 'dflqrtsv52tp2f',
+  username: 'arekrbzyzpvkvl',
+  password: '9e6890f12865a85527172a0bb5435fa1ed3a7621f3bfc2a639cb1187476057e5',
+  host: 'ec2-52-71-55-81.compute-1.amazonaws.com',
   dialect: 'postgres',
 };
 
